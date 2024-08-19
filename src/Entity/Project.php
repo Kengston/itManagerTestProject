026edeb0b00c380303c749f07bf14a05e1faa43f
Kengston <danidc@mail.ru>
2024-08-20 03:40:36 +0400

    tre-2: validation added

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -6,6 +6,7 @@ use App\Repository\ProjectRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
 class Project
@@ -16,9 +17,13 @@ class Project
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
     private ?string $client = null;
 
     #[ORM\OneToMany(mappedBy: 'project', targetEntity: Developer::class)]
